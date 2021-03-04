4d938194fbff20cf458c30695eb6ed0c50ae765c
PythonOS <1107056816@qq.com>
2021-03-04 15:12:26 -0500

    add seat

--- router/router.py
+++ router/router.py
@@ -4,8 +4,13 @@ __author__ = 'kevin'
 
 from tornado.web import url
 from controller.index import IndexHandler
-
+from controller.cinemas import CinemaHandler 
+from controller.cinemaDetail import CinemaDetailHandler
+from controller.seat_select import SeatSelectHandler
 
 Router = [
     url(r"/ajax/movieOnInfoList", IndexHandler),
+    url(r"/ajax/cinemaList", CinemaHandler),
+    url(r"/ajax/cinemaDetail", CinemaDetailHandler),
+    url(r"/ajax/seat_select", SeatSelectHandler),
 ]
